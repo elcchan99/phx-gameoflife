8a481dad944588fff7aa53484b145894037474e4
edgar-chan@ <edgar.chan@cloverhealth.com>
2021-03-28 12:45:11 +0800

    disable ecto

--- lib/gameoflife/application.ex
+++ lib/gameoflife/application.ex
@@ -8,7 +8,7 @@ defmodule Gameoflife.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      Gameoflife.Repo,
+      # Gameoflife.Repo,
       # Start the Telemetry supervisor
       GameoflifeWeb.Telemetry,
       # Start the PubSub system
