ad0023ae849ee298b6ff773f0c191ec51f1fcb47
edgar-chan@ <edgar.chan@cloverhealth.com>
2021-03-28 17:00:11 +0800

    fix component Cell

--- lib/gameoflife_web/components/board.ex
+++ lib/gameoflife_web/components/board.ex
@@ -10,7 +10,11 @@ defmodule GameoflifeWeb.Components.Board do
     ~H"""
       <div class="board">
         <div class="row row-{{row_index}}" :for={{ {row, row_index} <- (Enum.chunk_every(@value.state, @value.width) |> Enum.with_index()) }}>
-          <Cell state={{row}}/>
+          <Cell
+            :for={{ {cell, col_index} <- Enum.with_index(row)}}
+            class="col-{{col_index}}"
+            index={{row_index * @value.width + col_index}}
+            state={{cell}}/>
         </div>
       </div>
     """
--- lib/gameoflife_web/components/cell.ex
+++ lib/gameoflife_web/components/cell.ex
@@ -3,12 +3,18 @@ defmodule GameoflifeWeb.Components.Cell do
 
   alias Gameoflife.Game.Cell, as: CellStruct
 
-  prop state, :list, required: true
+  @doc "HTML Class"
+  prop class, :string, default: ""
+
+  @doc "Cell index"
+  prop index, :number, required: true
+
+  @doc "Cell state"
+  prop state, :module, required: true
 
   def render(assigns) do
     ~H"""
-    <div class="cell col-{{col_index}} {{render_cell_state(cell)}}"
-      :for={{ {cell, col_index} <- Enum.with_index(@state)}}>
+    <div class="cell {{@class}} {{render_cell_state(@state)}}">
     </div>
     """
   end
